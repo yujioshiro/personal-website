8153dad52f111bd441670ac03c6b1fe8cecc9780
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-27 01:46:39 -0700

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     title: `Yuji Oshiro`,
     siteUrl: `https://yujioshiro.com`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap",
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-netlify",
   {
     resolve: "gatsby-plugin-web-font-loader",
     options: {
