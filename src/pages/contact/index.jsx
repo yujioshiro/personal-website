dfac6c8edcd343be07a7b38a3c50f9e172e2c932
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-28 22:01:52 -0700

    fixed contact page again again

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -7,8 +7,9 @@ export default function IndexPage() {
     <Layout>
         <h1>contact me</h1>
         <form name="contact" method="POST" data-netlify="true">
-            <input type="text" id="name" name="name" placeholder="name" required/>
-            <input type="email" id="email" name="email" placeholder="email" required/>
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="text" id="name" name="name" placeholder="name" autoComplete='name' required/>
+            <input type="email" id="email" name="email" placeholder="email" autoComplete='email' required/>
             <textarea name="message" id="" cols="30" rows="10" placeholder="write your message here!" required></textarea>
             <input id="submit-button" type="submit" value="send"/>
         </form>
