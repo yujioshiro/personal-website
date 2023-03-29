ebff88b1c8455f2ab2d134a579e45f98f3aa8dd4
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-29 00:20:29 -0700

    uhhh

--- src/pages/playlists/index.jsx
+++ src/pages/playlists/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Layout from '../../components/Layout';
 
 export default function IndexPage() {
+
   return (
     <Layout>
-        <h1>music for me and you</h1>
+        <h1>music for me and you {process.env.GATSBY_THIS_IS_A_TEST_VARIABLE}</h1>
     </Layout>
   );
+
 }
