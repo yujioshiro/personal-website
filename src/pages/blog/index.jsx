12994973b12590dbe3d5ca0abf3bb10a02333bba
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-29 06:45:53 -0700

    fixed empty file

---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+// import '../../../static/stylesheets/contact.css'
+
+export default function IndexPage() {
+  return (
+    <Layout>
+        <h1>blogs</h1>
+
+    </Layout>
+  );
+}
