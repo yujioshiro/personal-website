e7abd84e466641f7140edfce668b3069f3086887
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-29 06:49:28 -0700

    fixed webpack error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,5 @@
 exports.onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
-        plugins: [
-            new webpack.ProvidePlugin({
-                Buffer: [require.resolve("buffer/"), "Buffer"],
-            }),
-        ],
       resolve: {
         fallback: {
           fs: false,
