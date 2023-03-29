c285f2b9ffbe324fe732e434afb08eb59076c365
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-29 06:40:03 -0700

    final fix to resume, promise

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -25,9 +25,9 @@ const Resume = () => {
 
   return (
     <Layout>
-      <div style={{ maxWidth: "700px", marginLeft:"auto",marginRight:"auto"}}>
-        <object ref={objectRef} data={`../resume/resume.pdf?v=${new Date().getTime()}`} type="application/pdf" width="100%">
-            <p>PDF cannot be displayed. Please download it instead.</p>
+      <div style={{ maxWidth: "700px", margin:"2vh auto",filter:"brightness(85%)"}}>
+        <object ref={objectRef} data={`../resume/YujiOshiroResume.pdf?v=${new Date().getTime()}`} type="application/pdf" width="100%">
+          <p>Unable to display PDF. Please <a href="../resume/YujiOshiroResume.pdf" title="Download YujiOshiroResume.pdf" download>download</a> it instead.</p>
         </object>
       </div>
 
