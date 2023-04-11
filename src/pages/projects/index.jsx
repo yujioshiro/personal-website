6c53c3b099145978c6c534d4ac5b632329ca9b9e
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-04-11 02:28:44 -0700

    Update projects

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -10,11 +10,11 @@ export default function ProjectsPage() {
       {
         projectsData.projects.map(project => (
           <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-            <article key={project.title}>
+            <article key={project.title} className="">
               <header>{project.liveLink === "N/A"?<h4>{project.title}</h4>:<h4><a href={project.liveLink}>{project.title}</a></h4>}</header>
             {/* <p dangerouslySetInnerHTML={{ __html: project.description }}>{project.description}</p> */}
-            <span dangerouslySetInnerHTML={{ __html: project.description }}></span>
-            <footer class="project-footer">
+            <span className="project-description" dangerouslySetInnerHTML={{ __html: project.description }}></span>
+            <footer className="project-footer">
               <a href={project.githubLink}>view on github</a>
               {project.article !== "N/A"?<a href={project.article}>read details</a>:null}
             </footer>
