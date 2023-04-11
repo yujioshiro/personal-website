5c8a95d544a0cfa1e249b6ff355f5a4129dd10d2
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-04-11 02:28:57 -0700

    add CSS

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import NavBar from './NavBar';
 import './../../static/stylesheets/pico-master/css/pico.css';
+import './../../static/stylesheets/my.css';
 // import './../../static/stylesheets/examples-master/bootstrap-grid/css/pico-bootstrap-grid.css';
-
 
 export default function Layout({ children }) {
   return (
