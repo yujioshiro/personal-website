ae3aa9b3fda5b636d628d4b3feb709fa0f624405
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-05-24 21:58:33 -0700

    Update home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,8 @@ const IndexPage = () => (
   <Layout>
     <div class="homepage-sections" id="homepage-about-section">
     <h1>yuji oshiro's website</h1>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;I am a second year computer science student, <a href="https://merchduck.com" target="_blank" rel="noopener">business owner</a>, and curious learner of all things around us. {/*Sometimes I <a href="./blog">write</a> about my findings!*/} My favorite hobby is talking to people and learning more about the human condition.</p>
+    <p>Second-year computer science student, part-time CS tutor, and serious learner of web development.{/*Sometimes I <a href="./blog">write</a> about my findings!*/} My favorite hobby is talking to people and learning more about the human condition.</p>
+    <p>I also <a href="https://merchduck.com" target="_blank" rel="noopener">print shirts</a> for hackathons and school clubs across the US!</p>
     </div>
 
     {/* <div class="homepage-sections" id="homepage-links-section">
@@ -49,9 +50,16 @@ const IndexPage = () => (
     <div class="homepage-sections" id="homepage-projects-section">
       <h2 class="main-page-subheadings">things I'm working on</h2>
       <ul class="main-page-descriptions">
+        <li><a href="https://courseguide.yujioshiro.com" target="_blank" rel="noopener">Course Guide</a></li>
+        <ul>
+          <li>This site will let students review their courses and professors</li>
+          <li>The frontend is built with React. The charts are created with VictoryCharts</li>
+          <li>Currently learning PostgreSQL to build a database to make multi-school possible</li>
+          <li>I will initially launch it to IVC and Saddleback College students and hope to turn it into an open-data project where students from other schools can help collect the course and professor information for their school</li>
+        </ul>
         <li><a href="https://playlistpal.yujioshiro.com" target="_blank" rel="noopener">PlaylistPal</a>, react, view <a href="https://github.com/yujioshiro/playlistpal">github repo</a></li>
-        <li><a href="https://merchduck.com" target="_blank" rel="noopener">merchduck</a>, vanilla js</li>
-        <li>this website, gatsby.js, view <a href="https://github.com/yujioshiro/personal-website">github repo</a></li>
+        {/* <li><a href="https://merchduck.com" target="_blank" rel="noopener">merchduck</a>, vanilla js</li> */}
+        {/* <li>this website, gatsby.js, view <a href="https://github.com/yujioshiro/personal-website">github repo</a></li> */}
         <li>view <a href="/projects">all projects</a></li>
       </ul>
     </div>
@@ -69,6 +77,12 @@ const IndexPage = () => (
       <li>to support a research group with programming and data processing tasks, <a href="/contact">recruit me</a></li>
       <ul>
         <li>preference for research groups working with natural world data, human population data, and space</li>
+      </ul>
+      <li>people to interview. If any of these describes you and you wouldn't mind sharing your story, I'd love to talk to you!</li>
+      <ul>
+        <li>immigrant (of any country)</li>
+        <li>have a big dream</li>
+        <li>are working with exclusive technology</li>
       </ul>
     </ul>
 </div>
