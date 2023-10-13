86624ff167df720928575c7c4eca3c19b5e0810a
Yuji Oshiro <iamyujioshiro@gmail.com>
2023-10-13 10:50:24 -0700

    Update meetup details

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -7,12 +7,12 @@ export default function ProjectsPage() {
         <h1><strong>Irvine Code & Coffee</strong></h1>
         <div class="">
           <article>
-            <h2>Next meetup: October 11th, 6pm</h2>
+            <h2>Next meetup: October 25th, 6pm</h2>
             <p>This is a casual meetup for developers and people working with/in software. Despite the name, we don't really code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
               <li>@ The Lost Bean, 4632 Barranca Pkwy, Irvine (in the Woodbridge Village Center)</li>
-              <li>we are part of Learn Teach Code LA, you can RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/296268224/" target='_blank'>here</a>!</li>
+              <li>we are part of Learn Teach Code LA, you can RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/296573385/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
                   <li>#irvine channel in the <a href="https://discord.gg/TnqPC9mFSf" target='_blank'>LTC Discord Server</a></li>
