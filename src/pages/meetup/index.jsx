b4a2fed38f1972bc8abf1c9e7cbd3f5040ead5b3
Yuji Oshiro <iamyujioshiro@gmail.com>
2023-11-23 01:36:47 -0800

    Update meetup details

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -7,12 +7,12 @@ export default function ProjectsPage() {
         <h1><strong>Irvine Code & Coffee</strong></h1>
         <div class="">
           <article>
-            <h2>Next meetup: November 22nd, 6pm</h2>
+            <h2>Next meetup: Wednesday, December 6th, 6pm</h2>
             <p>This is a casual meetup for developers. Students and others working adjacent to software are more than welcome to come! Despite the name, we don't really code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
               <li>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</li>
-              <li>we are part of Learn Teach Code LA, you can RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297130353/" target='_blank'>here</a>!</li>
+              <li>we are part of Learn Teach Code LA, you can RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297186898/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
                   <li>#irvine channel in the <a href="https://discord.gg/TnqPC9mFSf" target='_blank'>LTC Discord Server</a></li>
@@ -20,11 +20,9 @@ export default function ProjectsPage() {
                   <li>use my <a href="https://yujioshiro.com/contact/">contact page</a></li>
                 </ul>
               </li>
-              {/* <li>No, you do not have to be a working developer to join us. This meetup is open to engineers, salespersons, owners, students, and anyone generally interested in tech and software</li> */}
             </ul>
-            {/* <footer>This meetup is run under Learn Teach Code LA</footer> */}
           </article>
-    </div>
+        </div>
     </Layout>
   )
 }
