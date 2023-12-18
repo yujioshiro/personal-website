2ca25e1b5231a11a9464c35cc057a30c96e0e4c3
Yuji Oshiro <iamyujioshiro@gmail.com>
2023-12-17 18:28:54 -0800

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -11,7 +11,7 @@ export default function ProjectsPage() {
             <p>This is a casual meetup for developers. Students and others working adjacent to software are more than welcome to come! Despite the name, we don't really code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
-              <li>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</li>
+              <li>due to the forecasted rain, we will be meeting at Hangar 24 in Irvine this week, 17877 Von Karman Ave Unit 110, Irvine, CA 92614<strike>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</strike></li>
               <li>we are part of Learn Teach Code LA, you can RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297692498/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
