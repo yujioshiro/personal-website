b347d791a505cbe088cb3237a8a09d046ba54e4a
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-01-01 16:37:02 -0800

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -7,17 +7,16 @@ export default function ProjectsPage() {
         <h1><strong>Irvine Code & Coffee</strong></h1>
         <div class="">
           <article>
-            <h2>Next meetup: Wednesday, January 3rd, 6pm</h2>
+            <h2>Next meetup: Wednesday, January 3rd, 6pm (Location change due to rain)</h2>
             <p>This is a casual meetup for developers. Students and others working adjacent to software are more than welcome to come! Despite the name, we don't really code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
-              <li>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</li>
-              <li>we are part of Learn Teach Code LA, you can RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297954536/" target='_blank'>here</a>!</li>
+              <li>due to the rain, we will be meting at Back Street Brewery inside Lamppost Pizza in Irvine<s>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</s></li>
+              <li>find more info and RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297954536/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
-                  <li>#irvine channel in the <a href="https://discord.gg/TnqPC9mFSf" target='_blank'>LTC Discord Server</a></li>
+                  <li>#general channel in the <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Tech Link Up Discord Server</a></li>
                   <li>message me on <a href="https://www.linkedin.com/in/yujioshiro/" target='_blank'>LinkedIn</a></li>
-                  <li>use my <a href="https://yujioshiro.com/contact/">contact page</a></li>
                 </ul>
               </li>
             </ul>
