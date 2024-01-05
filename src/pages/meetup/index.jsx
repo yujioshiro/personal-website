db3162d3342b48029a83ccf299e57c67c50fffd5
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-01-05 10:18:21 -0800

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -7,12 +7,12 @@ export default function ProjectsPage() {
         <h1><strong>Irvine Code & Coffee</strong></h1>
         <div class="">
           <article>
-            <h2>Next meetup: Wednesday, January 3rd, 6pm (Location change due to rain)</h2>
+            <h2>Next meetup: Wednesday, January 17th, 6pm</h2>
             <p>This is a casual meetup for developers. Students and others working adjacent to software are more than welcome to come! Despite the name, we don't really code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
-              <li>due to the rain, we will be meeting at Back Street Brewery inside Lamppost Pizza in Irvine <s>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</s></li>
-              <li>find more info and RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297954536/" target='_blank'>here</a>!</li>
+              <li>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</li>
+              <li>RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297951794/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
                   <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
