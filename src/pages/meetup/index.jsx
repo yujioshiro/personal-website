be430cadaa932c8da73f63f4d59b4b2903f9fb97
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-01-08 12:48:59 -0800

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -11,7 +11,7 @@ export default function ProjectsPage() {
             <p>This is a casual meetup for developers. Students and others working adjacent to software are more than welcome to come! Despite the name, we don't really code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
-              <li>@ The center of the Woodbridge Village Center (The tables under the orange covers in front of Cha)</li>
+              <li>@ Backstreet Brewery (inside Lamppost Pizza), <a href="https://maps.app.goo.gl/HtSn53mtLLMgySENA">14450 Culver Dr, Irvine, CA 92604</a></li>
               <li>RSVP to the event <a href="https://www.meetup.com/learnteachcode/events/297951794/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
