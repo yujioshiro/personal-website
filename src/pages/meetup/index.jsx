1c4d502eef3819744fc006f6e8b13f2449d0ad48
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-02-29 18:15:51 -0800

    Update meetup information

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -4,15 +4,32 @@ import Layout from '../../components/Layout';
 export default function ProjectsPage() {
   return (
     <Layout>
-        <h1><strong>Irvine Code & Coffee</strong></h1>
+        <h1><strong>tech meetups yuji runs in orange county, ca</strong></h1>
+        <h2>Sunset Hike OR Bike with New Tech Friends</h2>
         <div class="">
           <article>
-            <h2>Next meetup: Wednesday, February 28th, 6pm</h2>
+            <h3>Next meetup: Saturday, March 9th, 4pm</h3>
+            <p>Come on a casual hike OR bike at the Newport Bay. You can join either the hiking group (3 mile walk around the upper bay) or the biking around (10 miles around the full Back bay loop).</p>
+            <ul>
+              <li>@ the accessible parking spots in the <a href="https://maps.app.goo.gl/mZSenC5Y7bm5ghNdA">Peter and Mary Muth Interpretive Center Parking Lot</a></li>
+              <li>More info and RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/299003219/" target='_blank'>here</a>!</li>
+              <li>for any questions:
+                <ul>
+                  <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+                </ul>
+              </li>
+            </ul>
+          </article>
+        </div>
+        <h2>Irvine Code and Coffee</h2>
+        <div class="">
+          <article>
+            <h3>Next meetup: Wednesday, March 13th, 6pm</h3>
             <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
               <li>on the patio @ Backstreet Brewery (inside Lamppost Pizza), <a href="https://maps.app.goo.gl/HtSn53mtLLMgySENA">14450 Culver Dr, Irvine, CA 92604</a></li>
-              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/298569360/" target='_blank'>here</a>!</li>
+              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/299223387/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
                   <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
