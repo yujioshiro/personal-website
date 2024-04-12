ccf7acd95e1a32c21e5d9d02d909a9be10070a61
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-04-11 23:37:03 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -24,12 +24,12 @@ export default function ProjectsPage() {
         <h2>Irvine Code and Coffee</h2>
         <div class="">
           <article>
-            <h3>Next meetup: Wednesday, April 10th, 6pm</h3>
+            <h3>Next meetup: Wednesday, April 24th, 6pm</h3>
             <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
               <li>@ Hangar 24 (exact location will be sent in the Discord), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/299716042" target='_blank'>here</a>!</li>
+              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/299716046/" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
                   <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
