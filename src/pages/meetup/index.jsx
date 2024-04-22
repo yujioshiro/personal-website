c8d92001e3ff00f5732bc0fa39acb5e3c0b3c1cf
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-04-22 00:06:45 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -41,6 +41,39 @@ export default function ProjectsPage() {
           </article>
         </div>
 
+        <h2>Community Collab Night</h2>
+        <div class="">
+          <article>
+            <h3>Next meetup: Thursday, May 2nd, 6pm</h3>
+            <p>This is an event for members of the community to share projects their working on or share ideas so they can find collaborators. Everyone is welcome! If you have a project, come share it. If you aren't currently working on anything outside of work, but want to see what the community is up to, come on by!</p>
+            <ul>
+              <li>@ The Duck Club (provided by the Irvine Ranch Water District), <a href="https://maps.app.goo.gl/GsyWRf6Bjfs6E4Fo9">15 Riparian View, Irvine, CA 92612</a></li>
+              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300162978" target='_blank'>here</a>!</li>
+              <li>for any questions:
+                <ul>
+                  <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+                </ul>
+              </li>
+            </ul>
+          </article>
+        </div>
+
+        <h2>Sunset Cruise with Friends of Figma!</h2>
+        <div class="">
+          <article>
+            <h3>Wednesday, May 29th, 7pm</h3>
+            <p>This event is in collaboration with Friends of Figma Irvine. We will be going on a cruise out of Newport Beach on a 100 person catamaran. Tickets are $29 until April 26th, after they will go up to $34. There is a lot more info in the meetup page, so please read through it!</p>
+            <ul>
+              <li>@ Newport Landing Whale Watching, <a href="https://maps.app.goo.gl/HZWi89K4ZEkCjf898">309 Palm St A, Newport Beach, CA 92661</a></li>
+              <li>RSVP to the event <a href="https://www.meetup.com/friends-of-figma-irvine-ux-design-group/events/300377382" target='_blank'>here</a>!</li>
+              <li>for any questions:
+                <ul>
+                  <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+                </ul>
+              </li>
+            </ul>
+          </article>
+        </div>
     </Layout>
   )
 }
