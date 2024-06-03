2902444b3b3e849975349ab67be57a8bc2be87de
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-06-03 01:22:04 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -43,12 +43,12 @@ export default function ProjectsPage() {
         <div class="">
           <article>
             <h2>Irvine Code and Coffee</h2>
-            <h3>Next meetup: Wednesday, May 22nd, 6pm</h3>
+            <h3>Next meetup: Wednesday, June 5th, 6pm</h3>
             <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
             <ul>
               <li>meet every other Wednesday at 6pm</li>
               <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300459716" target='_blank'>here</a>!</li>
+              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300939021" target='_blank'>here</a>!</li>
               <li>for any questions:
                 <ul>
                   <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
@@ -59,6 +59,23 @@ export default function ProjectsPage() {
         </div>
 
         <div class="">
+          <article>
+            <h2>Picnic and Board Games with New Tech Friends!!</h2>
+            <h3>Next meetup: Wednesday, June 8th, 11am</h3>
+            <p>Bring a lunch and your favorite board game and chill with us at Bill Barber Memorial Park.</p>
+            <ul>
+              <li>@ Bill Barber Memorial Park (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/u1JcjbxebcAZfHQ37">4 Civic Center Plaza, Irvine, CA 92606</a></li>
+              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300939021" target='_blank'>here</a>!</li>
+              <li>for any questions:
+                <ul>
+                  <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+                </ul>
+              </li>
+            </ul>
+          </article>
+        </div>
+
+        {/* <div class="">
           <article>
             <h2>Sunset Cruise with Friends of Figma!</h2>
             <h3>Wednesday, May 29th, 7pm</h3>
@@ -73,7 +90,7 @@ export default function ProjectsPage() {
               </li>
             </ul>
           </article>
-        </div>
+        </div> */}
     </Layout>
   )
 }
