3e9e7974fa535ac223172a2224bd932277aaa141
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-06-04 09:27:27 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -4,9 +4,9 @@ import Layout from '../../components/Layout';
 export default function ProjectsPage() {
   return (
     <Layout>
-        <h1><strong>tech meetups yuji helps run in orange county, ca</strong></h1>
+      <h1><strong>tech meetups yuji helps run in orange county, ca</strong></h1>
 
-        {/* <h2>Hike with New Tech Friends</h2>
+      {/* <h2>Hike with New Tech Friends</h2>
         <div class="">
           <article>
             <h3>Sunday, April 21st, 10am</h3>
@@ -23,59 +23,59 @@ export default function ProjectsPage() {
           </article>
         </div> */}
 
-        {/* <div class="">
-          <article>
-            <h2>Community Collab Night</h2>
-            <h3>Next meetup: Thursday, May 2nd, 6pm</h3>
-            <p>This is an event for members of the community to share projects their working on or share ideas so they can find collaborators. Everyone is welcome! If you have a project, come share it. If you aren't currently working on anything outside of work, but want to see what the community is up to, come on by!</p>
-            <ul>
-              <li>@ The Duck Club (provided by the Irvine Ranch Water District), <a href="https://maps.app.goo.gl/GsyWRf6Bjfs6E4Fo9">15 Riparian View, Irvine, CA 92612</a></li>
-              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300162978" target='_blank'>here</a>!</li>
-              <li>for any questions:
-                <ul>
-                  <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
-                </ul>
-              </li>
-            </ul>
-          </article>
-        </div> */}
+      <div class="">
+        <article>
+          <h2>Irvine Code and Coffee</h2>
+          <h3>Next meetup: Wednesday, June 5th, 6pm</h3>
+          <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
+          <ul>
+            <li>meet every other Wednesday at 6pm</li>
+            <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300520670" target='_blank'>here</a>!</li>
+            <li>for any questions:
+              <ul>
+                <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+              </ul>
+            </li>
+          </ul>
+        </article>
+      </div>
 
-        <div class="">
-          <article>
-            <h2>Irvine Code and Coffee</h2>
-            <h3>Next meetup: Wednesday, June 5th, 6pm</h3>
-            <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
-            <ul>
-              <li>meet every other Wednesday at 6pm</li>
-              <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300520670" target='_blank'>here</a>!</li>
-              <li>for any questions:
-                <ul>
-                  <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
-                </ul>
-              </li>
-            </ul>
-          </article>
-        </div>
+      <div class="">
+        <article>
+          <h2>Picnic and Board Games with New Tech Friends!!</h2>
+          <h3>Next meetup: Wednesday, June 8th, 11am</h3>
+          <p>Bring a lunch and your favorite board game and chill with us at Bill Barber Memorial Park.</p>
+          <ul>
+            <li>@ Bill Barber Memorial Park (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/u1JcjbxebcAZfHQ37">4 Civic Center Plaza, Irvine, CA 92606</a></li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300939021" target='_blank'>here</a>!</li>
+            <li>for any questions:
+              <ul>
+                <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+              </ul>
+            </li>
+          </ul>
+        </article>
+      </div>
 
-        <div class="">
-          <article>
-            <h2>Picnic and Board Games with New Tech Friends!!</h2>
-            <h3>Next meetup: Wednesday, June 8th, 11am</h3>
-            <p>Bring a lunch and your favorite board game and chill with us at Bill Barber Memorial Park.</p>
-            <ul>
-              <li>@ Bill Barber Memorial Park (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/u1JcjbxebcAZfHQ37">4 Civic Center Plaza, Irvine, CA 92606</a></li>
-              <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300939021" target='_blank'>here</a>!</li>
-              <li>for any questions:
-                <ul>
-                  <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
-                </ul>
-              </li>
-            </ul>
-          </article>
-        </div>
+      <div class="">
+        <article>
+          <h2>Community Collab Night</h2>
+          <h3>Next meetup: Wednesday, July 24th, 6pm</h3>
+          <p>This is an event for members of the community to share projects their working on or share ideas so they can find collaborators. Everyone is welcome! If you have a project, come share it. If you aren't currently working on anything outside of work, but want to see what the community is up to, come on by!</p>
+          <ul>
+            <li>@ The Duck Club (provided by the Irvine Ranch Water District), <a href="https://maps.app.goo.gl/GsyWRf6Bjfs6E4Fo9">15 Riparian View, Irvine, CA 92612</a></li>
+            {/* <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300162978" target='_blank'>here</a>!</li> */}
+            <li>for any questions:
+              <ul>
+                <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+              </ul>
+            </li>
+          </ul>
+        </article>
+      </div>
 
-        {/* <div class="">
+      {/* <div class="">
           <article>
             <h2>Sunset Cruise with Friends of Figma!</h2>
             <h3>Wednesday, May 29th, 7pm</h3>
