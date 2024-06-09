03463f6f59bf992c86ea5510140772fe2546d992
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-06-09 15:17:00 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -26,12 +26,12 @@ export default function ProjectsPage() {
       <div class="">
         <article>
           <h2>Irvine Code and Coffee</h2>
-          <h3>Next meetup: Wednesday, June 5th, 6pm</h3>
+          <h3>Next meetup: Wednesday, June 19th, 6pm</h3>
           <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
           <ul>
             <li>meet every other Wednesday at 6pm</li>
             <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300520670" target='_blank'>here</a>!</li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300662990" target='_blank'>here</a>!</li>
             <li>for any questions:
               <ul>
                 <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
@@ -41,7 +41,7 @@ export default function ProjectsPage() {
         </article>
       </div>
 
-      <div class="">
+      {/* <div class="">
         <article>
           <h2>Picnic and Board Games with New Tech Friends!!</h2>
           <h3>Next meetup: Wednesday, June 8th, 11am</h3>
@@ -56,7 +56,7 @@ export default function ProjectsPage() {
             </li>
           </ul>
         </article>
-      </div>
+      </div> */}
 
       <div class="">
         <article>
