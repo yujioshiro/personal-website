e0570a535d44a3ab2ad2aa788267f6fe41b6fce3
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-07-10 16:37:16 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -26,12 +26,12 @@ export default function ProjectsPage() {
       <div class="">
         <article>
           <h2>Irvine Code and Coffee</h2>
-          <h3>Next meetup: Wednesday, July 3rd, 6pm</h3>
+          <h3>Next meetup: Wednesday, July 17th, 6pm</h3>
           <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
           <ul>
             <li>meet every other Wednesday at 6pm</li>
             <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/301477399/" target='_blank'>here</a>!</li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/301892346" target='_blank'>here</a>!</li>
             <li>for any questions:
               <ul>
                 <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
@@ -65,7 +65,7 @@ export default function ProjectsPage() {
           <p>This is an event for members of the community to share projects their working on or share ideas so they can find collaborators. Everyone is welcome! If you have a project, come share it. If you aren't currently working on anything outside of work, but want to see what the community is up to, come on by!</p>
           <ul>
             <li>@ The Duck Club (provided by the Irvine Ranch Water District), <a href="https://maps.app.goo.gl/GsyWRf6Bjfs6E4Fo9">15 Riparian View, Irvine, CA 92612</a></li>
-            {/* <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/300162978" target='_blank'>here</a>!</li> */}
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/301905517/" target='_blank'>here</a>!</li>
             <li>for any questions:
               <ul>
                 <li>#general channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
