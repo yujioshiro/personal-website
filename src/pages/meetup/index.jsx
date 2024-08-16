6c56bfe03be2ec378944a3c371a1617e5f11da11
Yuji Oshiro <iamyujioshiro@gmail.com>
2024-08-15 17:20:28 -0700

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -26,12 +26,29 @@ export default function ProjectsPage() {
       <div class="">
         <article>
           <h2>Irvine Code and Coffee</h2>
-          <h3>Next meetup: Wednesday, August 14th, 6pm</h3>
+          <h3>Next meetup: Wednesday, August 28th, 6pm</h3>
           <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
           <ul>
             <li>meet every other Wednesday at 6pm</li>
             <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/302129117/" target='_blank'>here</a>!</li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/302129129" target='_blank'>here</a>!</li>
+            <li>for any questions:
+              <ul>
+                <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
+              </ul>
+            </li>
+          </ul>
+        </article>
+      </div>
+
+      <div class="">
+        <article>
+          <h2>Resume Review Night</h2>
+          <h3>Next meetup: Tuesday, September 3rd, 6pm</h3>
+          <p>Get your resume reviewed by experienced professionals!</p>
+          <ul>
+            <li>@ The Duck Club (provided by the Irvine Ranch Water District), <a href="https://maps.app.goo.gl/GsyWRf6Bjfs6E4Fo9">15 Riparian View, Irvine, CA 92612</a></li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/302675050" target='_blank'>here</a>!</li>
             <li>for any questions:
               <ul>
                 <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
