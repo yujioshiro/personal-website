3414b8ae4c4746dd45ae43b7904e008307349f35
Yuji Oshiro <iamyujioshiro@gmail.com>
2025-01-22 07:20:49 -0800

    Update meetup info

--- src/pages/meetup/index.jsx
+++ src/pages/meetup/index.jsx
@@ -6,7 +6,7 @@ export default function ProjectsPage() {
     <Layout>
       <h1><strong>tech meetups yuji helps run in orange county, ca</strong></h1>
 
-        <div class="">
+        {/* <div class="">
           <article>
             <h2>New Year Hike with Tech Friends</h2>
             <h3>Sunday, January 5th, 10am</h3>
@@ -21,17 +21,17 @@ export default function ProjectsPage() {
               </li>
             </ul>
           </article>
-        </div>
+        </div> */}
 
       <div class="">
         <article>
           <h2>Irvine Code and Coffee</h2>
-          <h3>Next meetup: Wednesday, January 15th, 6pm</h3>
+          <h3>Next meetup: Wednesday, January 29th, 6pm</h3>
           <p>This is a casual meetup for developers, designers, and everyone in tech! Despite the name, we don't code at these meetups; we like to talk, meet new friends, and make new connections.</p>
           <ul>
             <li>meet every other Wednesday at 6pm</li>
             <li>@ Hangar 24 (exact location will be sent in the Discord on the day of the event), <a href="https://maps.app.goo.gl/SUDHHD31Qq2J8Yqc7">17877 Von Karman Ave Unit 110, Irvine, CA 92614</a></li>
-            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/303290638" target='_blank'>here</a>!</li>
+            <li>RSVP to the event <a href="https://www.meetup.com/oc-tech-link-up/events/304333049" target='_blank'>here</a>!</li>
             <li>for any questions:
               <ul>
                 <li>#questions channel in the Tech Link Up <a href="https://discord.com/channels/1187489446245961748/1187489446245961751" target='_blank'>Discord Server</a></li>
