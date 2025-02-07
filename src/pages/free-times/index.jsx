376daed945c0e747598a59cacdb49d72f7bc0266
Yuji Oshiro <iamyujioshiro@gmail.com>
2025-02-07 09:27:08 -0800

    update calendar style

--- src/pages/free-times/index.jsx
+++ src/pages/free-times/index.jsx
@@ -7,7 +7,7 @@ export default function FreeTimesPage() {
       <iframe 
         src="https://calendar.google.com/calendar/embed?src=648eb580a538908ad5db65109eb362990ce4e4b22ca1895285f4e6e65101a34b%40group.calendar.google.com&ctz=America%2FLos_Angeles"
         style={{ border: "0" }} 
-        width="800" 
+        width="100%" 
         height="600" 
         frameBorder="0" 
         scrolling="no">
