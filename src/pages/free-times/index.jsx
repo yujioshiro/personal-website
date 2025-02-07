0d08aab5fbb82d85f5414290b95f6129de7cabf1
Yuji Oshiro <iamyujioshiro@gmail.com>
2025-02-07 02:01:21 -0800

    update calendar

--- src/pages/free-times/index.jsx
+++ src/pages/free-times/index.jsx
@@ -4,8 +4,14 @@ import Layout from '../../components/Layout';
 export default function FreeTimesPage() {
   return (
     <Layout>
-      <h1>my schedule</h1>
-      <iframe src="https://calendar.google.com/calendar/embed?src=648eb580a538908ad5db65109eb362990ce4e4b22ca1895285f4e6e65101a34b%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <iframe 
+        src="https://calendar.google.com/calendar/embed?src=648eb580a538908ad5db65109eb362990ce4e4b22ca1895285f4e6e65101a34b%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+        style={{ border: "0" }} 
+        width="800" 
+        height="600" 
+        frameBorder="0" 
+        scrolling="no">
+      </iframe>
     </Layout>
   )
 }
