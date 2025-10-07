0b772f34c6105e43461afbe4affda99e76fa5c87
Yuji Oshiro <iamyujioshiro@gmail.com>
2025-10-06 20:06:47 -0700

    Remove meetup info

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,12 +11,12 @@ const IndexPage = () => (
     <h1>yuji oshiro's website</h1>
     <p>software engineer @ Pacific Life, fourth-year computer science student @ Oregon State University, part-time CS tutor. My favorite hobby is talking to people and learning more about the human condition.</p>
     </div>
-    <div class="homepage-sections" id="homepage-projects-section">
+    {/* <div class="homepage-sections" id="homepage-projects-section">
       <h2 class="main-page-subheadings">things I do</h2>
       <ul class="main-page-descriptions">
         <li>ALWAYS trying to build more opportunities for people in Orange County to meet new people and find new friends. Check out some of the events I run <a href="https://yujioshiro.com/meetup/">here</a>!</li>
       </ul>
-    </div>
+    </div> */}
   </Layout>
 );
 
